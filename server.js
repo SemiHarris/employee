7cf8c336fc0dfc2016dab4e3344de6a96adbf5aa
Semi <Semiharris3@gmail.com>
2022-02-28 10:37:15 -0500

    Added ability to post employee data thourgh inquerier

--- server.js
+++ server.js
@@ -11,21 +11,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', apiRoutes);
 
-process.on('uncaughtException', function (err) {
-  console.log(err);
-})
+const employeeQuestions = [{
+  type: 'input'
+
+}]
 
 app.use((req, res) => {
     res.status(404).end();
 });
 
-const getEmployee = () =>
-  fetch('http://localhost:3001/api/employee', {
+const getData = (query) =>
+  fetch(('http://localhost:3001/api/' + query), {
     method: 'GET',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
+});
+
+const postData = (query, data) =>
+  fetch(('http://localhost:3001/api/' + query), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify(data),
 });
 
 const addData = () => {
@@ -35,7 +45,35 @@ const addData = () => {
       name: 'add',
       message: 'What database would you like to add to?(employee, role, or department)'
   }
-]).then(result => {console.log(result.add)})
+]).then(result => {
+  if (result.add = 'employee') {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'first_name',
+        message: 'What is the employees first name?'
+      },
+      {
+        type: 'input',
+        name: 'last_name',
+        message: 'What is the employees last name?'
+      },
+      {
+        type: 'number',
+        name: 'role_id',
+        message: 'What is the role ID of the employee?'
+      },
+      {
+        type: 'number',
+        name: 'manager_id',
+        message: 'What is the mangers ID?(If none leave blank)'
+      }
+    ]).then(result => {
+      console.log(result)
+      postData('employee', result)
+    })
+  }
+})
 }
 
 const question = () => {
@@ -43,7 +81,7 @@ const question = () => {
     {
       type: 'input',
       name: 'choice',
-      message: 'Would you like to ADD, UPDATE, or DELETE a database?'
+      message: 'Would you like to ADD, UPDATE, DELETE, or VIEW a database?'
     }
   ]).then(x => {
     if(x.choice = 'Add' || x.choice === 'ADD' || x.choice === 'add'){
@@ -62,7 +100,8 @@ const displayEmployee = async (worker) => {
 }
 
 
-const renderEmployee = () => getEmployee().then(displayEmployee);
+const renderEmployee = () => getData('employee').then(displayEmployee);
+
 
 
   db.connect(err => {
